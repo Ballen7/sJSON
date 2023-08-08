465373873b83a67f3dddbe51d1a15145d9caf701
material <Intellgb@gmail.com>
2023-08-07 21:12:22 -0400

    Update sjson.c

--- sjson.c
+++ sjson.c
@@ -25,7 +25,7 @@
 #include "sjson.h"
 
 #define SJSON_WRITE_ADDRESS(ctx) ((char *)&ctx->pBuf[ctx->index])
-#define SJSON_AVAIABLE_SPACE(ctx) ((size_t)(ctx->buf_size - ctx->index))
+#define SJSON_AVAILABLE_SPACE(ctx) ((size_t)(ctx->buf_size - ctx->index))
 
 static const uint8_t *const SJSON_TRUE_STRING = (const uint8_t *)"true";
 static const uint8_t *const SJSON_FALSE_STRING = (const uint8_t *)"false";
@@ -33,7 +33,7 @@ static const uint8_t *const SJSON_FALSE_STRING = (const uint8_t *)"false";
 static sjson_retval_t sjson_initate_keypair(sjson_context_t *ctx) {
   if (ctx->pBuf == NULL) return SJSON_ERROR;
   if (ctx->buf_size == 0) return SJSON_ERROR;
-  if (SJSON_AVAIABLE_SPACE(ctx) == 0) return SJSON_ERROR;
+  if (SJSON_AVAILABLE_SPACE(ctx) == 0) return SJSON_ERROR;
   if (ctx->state != SJSON_NEXT_KEY_PAIR && ctx->state != SJSON_START)
     return SJSON_ERROR;
 
@@ -44,7 +44,7 @@ static sjson_retval_t sjson_initate_keypair(sjson_context_t *ctx) {
 
 static sjson_retval_t sjson_add_key(sjson_context_t *ctx, uint8_t *key,
                                     size_t key_len) {
-  if (SJSON_AVAIABLE_SPACE(ctx) < 1 + key_len + 1 + 1) return SJSON_ERROR;
+  if (SJSON_AVAILABLE_SPACE(ctx) < 1 + key_len + 1 + 1) return SJSON_ERROR;
 
   ctx->pBuf[ctx->index++] = '"';
   memcpy(&ctx->pBuf[ctx->index], (void *)key, key_len);
@@ -58,28 +58,28 @@ static sjson_retval_t sjson_add_key(sjson_context_t *ctx, uint8_t *key,
 static sjson_retval_t int8_handler(sjson_context_t *ctx, void *value) {
   uint8_t _val = (uint8_t) * (uint8_t *)value;
   ctx->index += (int32_t)SJSON_SNPRINTF(SJSON_WRITE_ADDRESS(ctx),
-                                        SJSON_AVAIABLE_SPACE(ctx), "%u", _val);
+                                        SJSON_AVAILABLE_SPACE(ctx), "%u", _val);
   return (ctx->index > ctx->buf_size) ? SJSON_ERROR : SJSON_SUCCESS;
 }
 
 static sjson_retval_t int16_handler(sjson_context_t *ctx, void *value) {
   uint16_t _val = (uint16_t) * (uint16_t *)value;
   ctx->index += (int32_t)SJSON_SNPRINTF(SJSON_WRITE_ADDRESS(ctx),
-                                        SJSON_AVAIABLE_SPACE(ctx), "%u", _val);
+                                        SJSON_AVAILABLE_SPACE(ctx), "%u", _val);
   return (ctx->index > ctx->buf_size) ? SJSON_ERROR : SJSON_SUCCESS;
 }
 
 static sjson_retval_t int32_handler(sjson_context_t *ctx, void *value) {
   uint32_t _val = (uint32_t) * (uint32_t *)value;
   ctx->index += (int32_t)SJSON_SNPRINTF(SJSON_WRITE_ADDRESS(ctx),
-                                        SJSON_AVAIABLE_SPACE(ctx), "%u", _val);
+                                        SJSON_AVAILABLE_SPACE(ctx), "%u", _val);
   return (ctx->index > ctx->buf_size) ? SJSON_ERROR : SJSON_SUCCESS;
 }
 
 static sjson_retval_t int64_handler(sjson_context_t *ctx, void *value) {
   uint64_t _val = (uint64_t) * (uint64_t *)value;
   ctx->index += (int32_t)SJSON_SNPRINTF(SJSON_WRITE_ADDRESS(ctx),
-                                        SJSON_AVAIABLE_SPACE(ctx), "%lu", _val);
+                                        SJSON_AVAILABLE_SPACE(ctx), "%lu", _val);
   return (ctx->index > ctx->buf_size) ? SJSON_ERROR : SJSON_SUCCESS;
 }
 
@@ -108,7 +108,7 @@ sjson_retval_t sjson_add_string(sjson_context_t *ctx, uint8_t *key,
   ctx->state = SJSON_IN_PROGRESS;
 
   if (SJSON_SUCCESS != sjson_add_key(ctx, key, key_len)) return SJSON_ERROR;
-  if (SJSON_AVAIABLE_SPACE(ctx) < 1 + value_len + 1) return SJSON_ERROR;
+  if (SJSON_AVAILABLE_SPACE(ctx) < 1 + value_len + 1) return SJSON_ERROR;
 
   ctx->pBuf[ctx->index++] = '"';
   memcpy(&ctx->pBuf[ctx->index], value, value_len);
@@ -133,7 +133,7 @@ sjson_retval_t sjson_add_boolean(sjson_context_t *ctx, uint8_t *key,
     pStr = SJSON_TRUE_STRING;
   else
     pStr = SJSON_FALSE_STRING;
-  if (SJSON_AVAIABLE_SPACE(ctx) < strlen((const char *)pStr))
+  if (SJSON_AVAILABLE_SPACE(ctx) < strlen((const char *)pStr))
     return SJSON_ERROR;
 
   memcpy(&ctx->pBuf[ctx->index], pStr, strlen((const char *)pStr));
@@ -150,7 +150,7 @@ sjson_retval_t sjson_add_object(sjson_context_t *ctx, uint8_t *key,
   ctx->state = SJSON_IN_PROGRESS;
 
   if (SJSON_SUCCESS != sjson_add_key(ctx, key, key_len)) return SJSON_ERROR;
-  if (SJSON_AVAIABLE_SPACE(ctx) < value_len) return SJSON_ERROR;
+  if (SJSON_AVAILABLE_SPACE(ctx) < value_len) return SJSON_ERROR;
 
   memcpy(&ctx->pBuf[ctx->index], value, value_len);
   ctx->index += value_len;
@@ -176,7 +176,7 @@ sjson_retval_t sjson_init(sjson_context_t *ctx, uint8_t *buf, size_t buf_size) {
 
 sjson_retval_t sjson_close(sjson_context_t *ctx) {
   if (ctx->state != SJSON_NEXT_KEY_PAIR) return SJSON_ERROR;
-  if (SJSON_AVAIABLE_SPACE(ctx) < 1 + 1) return SJSON_ERROR;
+  if (SJSON_AVAILABLE_SPACE(ctx) < 1 + 1) return SJSON_ERROR;
 
   ctx->pBuf[ctx->index++] = '}';
   ctx->pBuf[ctx->index++] = 0;
